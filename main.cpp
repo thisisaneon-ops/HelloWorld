d83ca58c2b616bf1593f98cea1d59331a55949d0
N.Cheung <thisisaneon@gmail.com>
2026-04-08 20:51:00 +0800

    实现冒泡排序算法函数

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 void sort(int arr[], const int size)
 {
-
+	int tmp = 0;
+	for(int i = 0; i < size - 1; i++)
+	{
+		for(int j =0 ; j < size - i - 1; j++)
+		{
+			if(arr[j] > arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
 }
 
 int main()
@@ -12,5 +23,10 @@ int main()
 	int arr[] = {12,4,89,43,21,78};
 	const int size = sizeof(arr) / sizeof(arr[0]);
 	sort(arr, size);
+	for(auto& u : arr)
+	{
+		cout << u << " ";
+	}
 	return 0;
+	
 }
